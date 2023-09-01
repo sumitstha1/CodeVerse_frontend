3eaf3b77a5206f0daaf7ac9a91741e53c959ab56
sumitstha1 <edchristian.inventor@gmail.com>
2023-09-01 11:26:39 +0545

    Recent blog card stying to be done

--- components/Blogs/RecentBlog.jsx
+++ components/Blogs/RecentBlog.jsx
@@ -9,9 +9,11 @@ const RecentBlog = () => {
             <h2 className={`text-3xl font-semibold ${style.recentHeading}`}>Recent <span className='text-purple-400'>Blogs</span></h2>
             <div className={style.blogList}>
                 <article className={style.blogCard}>
-                    <Image src={"/choosing.jpg"} alt='Image' height={1000} width={2000} className={style.blogImage} />
+                    <div className={style.blogImage}>
+                        <Image src={"/choosing.jpg"} alt='Image' height={1000} width={2000} />
+                    </div>
                     <div className={style.blogDetail}>
-                        <h3 className={style.blogTitle}>Title</h3>
+                        <h3 className={`font-semibold text-2xl ${style.blogTitle}`}>Title</h3>
                         <div className={style.blogDesc}>
                             <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veritatis ratione eum, mollitia tempora minus fugiat, quaerat facilis et perferendis quibusdam possimus, rerum cupiditate est nemo!</p>
                             <Link href={"/"} className={style.blogLink}>Read More</Link>
