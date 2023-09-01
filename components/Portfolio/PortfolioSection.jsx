5eb854e01adefede9cc4ad6d6a07bf85110df629
sumitstha1 <edchristian.inventor@gmail.com>
2023-09-01 16:50:13 +0545

    Portfolio Added and connected with api

--- components/Portfolio/PortfolioSection.jsx
+++ components/Portfolio/PortfolioSection.jsx
@@ -1,15 +1,15 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
-import { portfolios } from '../../Data/portfolios'
+// import { portfolios } from '../../Data/portfolios'
 
-const PortfolioSection = () => {
+const PortfolioSection = ({ portfolios }) => {
     return (
         <div>
             <div className='flex justify-center items-center w-full'>
                 <div className='w-[90%]'>
                     <h1 className='text-4xl tracking-wider text-purple-500'>Portfolio</h1>
-                    <p className='text-base text-gray-500 mt-3'>This is a short description about the portfolio</p>
+                    <p className='text-base text-gray-500 mt-3'>&quot;Unlocking Online Success Together&quot;</p>
                 </div>
             </div>
             {/* <div className='md:flex justify-center'>
@@ -29,6 +29,7 @@ const PortfolioSection = () => {
 
             <div className='flex items-center flex-col md:flex-row md:mx-10 md:max-w-[100vw] md:grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3'>
                 {portfolios.map((e) => {
+                    console.log(e.image)
                     // console.log(process.env.WEB_URL + e.image)
                     return (
                         <div key={e.uid} className='w-[95%] my-5 md:w-full md:mx-2 flex-wrap'>
@@ -40,7 +41,7 @@ const PortfolioSection = () => {
                             <div className='my-6 p-2'>
                                 <h1 className='text-xl font-semibold text-gray-700'>{e.title}</h1>
                             </div>
-                            <Link href={e.url} target='_blank' className='border border-purple-500 text-purple-500 px-8 py-2 hover:bg-purple-500 hover:text-white transition ease-linear delay-75 duration-150 m-2'>View site</Link>
+                            <Link href={e.link} target='_blank' className='border border-purple-500 text-purple-500 px-8 py-2 hover:bg-purple-500 hover:text-white transition ease-linear delay-75 duration-150 m-2'>View site</Link>
                         </div>
                     )
                 })}
--- src/pages/portfolio/index.js
+++ src/pages/portfolio/index.js
@@ -1,12 +1,22 @@
 import React from 'react'
 import PortfolioSection from '../../../components/Portfolio/PortfolioSection'
 import UnderMaintenance from '../../../components/Utils/UnderMaintenance'
+import MetaHead from '../../../components/Meta/MetaTagHeader'
 
-const Portfolio = () => {
+const Portfolio = ({ portfolios }) => {
+  // console.log(portfolios)
   return (
     <>
-    {/* <UnderMaintenance /> */}
-    <PortfolioSection />
+      <MetaHead
+        title={"Portfolio - CodeVerse | Your Website Development Partner"}
+        meta_author={"CodeVerse"}
+        meta_keyword={"Website Development, Web Design, Web Development, Portfolio, Website Design, Responsive Design, Custom Web Development, Mobile-Friendly Websites, E-commerce Websites, CMS Websites, WordPress Development, Joomla Development, Drupal Development, Website Redesign, UI/UX Design, Front-End Development, Back-End Development, PHP Development, HTML/CSS, JavaScript, User Interface, User Experience, Web App Development, Mobile App Development, SEO-Friendly Websites, Graphic Design, Logo Design, Branding, Corporate Identity, Landing Pages, Blog Design, Social Media Integration, Website Maintenance, Website Security, Hosting Services, Domain Registration, Online Marketing, Content Management, E-commerce Solutions, Payment Gateway Integration, Online Booking Systems, Membership Websites, Non-Profit Websites, Educational Websites, Healthcare Websites, Government Websites, Restaurant Websites, Portfolio Websites, Photography Websites, Artist Portfolios, Event Websites, Real Estate Websites, Travel Websites, Fashion Websites, Technology Websites, Financial Websites, News Websites, E-learning Platforms, Video Streaming, Podcast Integration, Responsive Testing, Cross-Browser Compatibility, Speed Optimization, Website Analytics, Code Optimization, Security Audits, Website Consultation, Custom Features, Web Forms, Contact Forms, User Registration, Data Encryption, SSL Certificates, Content Delivery, API Integration, CRM Integration, Newsletter Signup, Testimonials, Case Studies, Client Projects, Creative Designs, Award-Winning Websites, Innovative Solutions, Client Satisfaction, High-Quality Work, Web Development Services, Professional Web Design, Digital Marketing, Online Presence, Dynamic Websites, Static Websites, Interactive Web Design, Modern Website Trends, SEO Strategies, Social Media Marketing, Digital Advertising, Search Engine Optimization, Blog Integration, Portfolio Showcases, Web Development Agency, Creative Web Solutions, Responsive Web Design, Mobile Optimization"}
+        meta_title={"Home - CodeVerse | Your Website Development Partner"}
+        meta_description={"Explore CodeVerse Nepal's portfolio of stunning web development and design projects. Our diverse range of work includes custom web development, responsive design, e-commerce solutions, and more, showcasing our commitment to innovation and excellence."}
+        meta_image={'/code.jpg'}
+      />
+      {/* <UnderMaintenance /> */}
+      <PortfolioSection portfolios={portfolios} />
     </>
   )
 }
@@ -15,24 +25,20 @@ export default Portfolio
 
 export async function getStaticProps(context) {
   try {
-    const portfolio_d = await fetch(`${process.env.API_URL}/portfolio/`);
-    const portfolio = await blog_d.json();
+    const portfolio_d = await fetch(`${process.env.API_URL}/account/portfolio/`);
+    const portfolios = await portfolio_d.json();
 
     return {
       props: {
-        blog: blog,
-        blogs: blogs,
-        slug: slug,
+        portfolios: portfolios,
       },
-      notFound: false,
     };
 
   }
   catch (error) {
     return {
       props: {
-        blog: {},
-        blogs: [],
+        portfolios: [],
       }
     }
   }
